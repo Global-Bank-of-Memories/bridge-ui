ba9e5791303e6cbce98bdec3b36a15e11c5179f6
yaroslavz <yaroslav.zakharov@door3.com>
2023-09-01 11:07:49 +0300

    update on new infrastructure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
 	gbmApiUrl: 'https://api.bankofmemories.org',
 	stellar: 'https://bankofmemories.org:8000',
 	bridge: ['https://api.bridge.bankofmemories.org'],
-	concordiumService: 'https://concordium-service.bridge.bankofmemories.org',
+	concordiumService: 'https://ccd-node-service.bridge.stage.bankofmemories.org',
 	web3Src: 'https://cdn.jsdelivr.net/npm/web3@1.3.5/dist/web3.min.js',
 	googleAnalyticsKey: 'G-M8PM6C2ZRQ'
 };
